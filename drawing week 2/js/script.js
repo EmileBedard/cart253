9cfda3bf221fbb513425058018282642250cb95f
EmileBedard <emile.bed@outlook.fr>
2025-09-08 14:47:08 -0400

    created the pink mirror and commented the code

--- drawing week 2/js/script.js
+++ drawing week 2/js/script.js
@@ -1,5 +1,5 @@
 /**
- * Drawing is really fun!
+ * The empty mirror
  * by Emile Bedard
  * 
  * Learning and drawing in p5!
@@ -20,5 +20,23 @@ function setup() {
  * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
 */
 function draw() {
+    // creates a beautiful teel background
+    background('#B2DBBF')
+
+    // create a pink ellipse with no outline
+    // representing the pink frame of an oval mirror
+    push();
+    noStroke();
+    fill(255, 70, 150);
+    ellipse(320, 320, 400, 550);
+    pop();
+
+    // creates the empty mirror glass
+    push();
+    fill(255, 255, 255);
+    stroke('#000000');
+    ellipse(320, 320, 370, 520);
+    pop();
+
 
 }
