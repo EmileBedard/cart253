737cd7dcec4a7d91eac5a7134bd8b2d3b35f47c9
EmileBedard <emile.bed@outlook.fr>
2025-09-08 14:24:29 -0400

    commented the project file for specificity

--- function calls week 2/template-p5-project/js/script.js
+++ function calls week 2/template-p5-project/js/script.js
@@ -1,21 +1,31 @@
 /**
- * Function Calls
+ * The Blank Page
  * Émile Bédard
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * creating a blank page simulation.
+ * 
+ * the program is **ininteractive** by default
+ * to convey the exsistential dread.
  */
 
 "use strict";
 
-
+/**
+ * creates the canvas for the blank page simulation
+ */
 function setup() {
     // happens once at the beginning
+    // creates the standard resolution canvas
     createCanvas(640, 480,);
 }
-
+/**
+ * draws the scary blank page
+ */
 function draw() {
     // happens every frame! ;)
+    // created the yellow desk
     background(255, 255, 120);
+    // the blank piece of paper
     rect(180, 120, 300, 300,)
+
 }
