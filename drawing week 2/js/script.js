0a3a8b73ab7a21acdacb3700e2ce79176a610294
EmileBedard <emile.bed@outlook.fr>
2025-09-08 14:32:26 -0400

    added title and top description

---
+++ drawing week 2/js/script.js
@@ -0,0 +1,24 @@
+/**
+ * Drawing is really fun!
+ * by Emile Bedard
+ * 
+ * Learning and drawing in p5!
+ */
+
+"use strict";
+
+/**
+ * OH LOOK I DIDN'T DESCRIBE SETUP!!
+*/
+function setup() {
+    createCanvas(640, 640,)
+
+}
+
+
+/**
+ * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+*/
+function draw() {
+
+}
