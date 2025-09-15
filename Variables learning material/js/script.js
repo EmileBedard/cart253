24dc79c0644a585527a0d36dccac396769f97baa
EmileBedard <emile.bed@outlook.fr>
2025-09-15 09:29:32 -0400

    added a rectangle following the mouse coordinates on X and a black background

--- Variables learning material/js/script.js
+++ Variables learning material/js/script.js
@@ -8,16 +8,25 @@
 "use strict";
 
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+ * draws a canvas
 */
 function setup() {
+    createCanvas(480, 480);
 
 }
 
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ * draws a rectangle in the center
 */
 function draw() {
+    background(0);
+
+    // draws the rectangle
+    push();
+    fill(255, 255, 255)
+    stroke(5);
+    rect(mouseX, height / 2, 300, 200);
+    pop();
 
 }
