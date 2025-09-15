9049cd71aa5ac91ba7d5fcda711a88a3f185fe05
EmileBedard <emile.bed@outlook.fr>
2025-09-15 08:53:57 -0400

    Named and described intial project for variables learning material

---
+++ Variables learning material/js/script.js
@@ -0,0 +1,23 @@
+/**
+ * Variables learning material
+ * By Emile Bedard
+ * 
+ * This project is about learning to work with variables.
+ */
+
+"use strict";
+
+/**
+ * OH LOOK I DIDN'T DESCRIBE SETUP!!
+*/
+function setup() {
+
+}
+
+
+/**
+ * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+*/
+function draw() {
+
+}
