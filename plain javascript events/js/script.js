5069a13f54d218639e514c0c1102d88c23c9a23b
EmileBedard <emile.bed@outlook.fr>
2025-09-30 16:16:36 -0400

    started the event debugging project

--- plain javascript events/js/script.js
+++ plain javascript events/js/script.js
@@ -1,24 +1,47 @@
 /**
- * Title of Project
- * Author Name
- * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * plain events experimenting with javascript
+ * by emile bedard
  */
 
 "use strict";
 
+const bg = {
+    fill: "#000000",
+    fills: {
+        black: "#000000",
+        white: "#ffffff",
+    },
+    switchKey: 32
+}
+
+
+
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+ * 
 */
 function setup() {
+    createCanvas(400, 400);
 
+
+    window.addEventListener("keyDown", changeBG);
 }
 
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ *
 */
 function draw() {
+    background(bg.fill);
+}
 
+function changeBG(event) {
+    if (event.keyCode === bg.switchKey) {
+        if (bg.fill === bg.fills.black) {
+            bg.fill = bg.fills.white;
+        }
+
+        else {
+            bg.fill = bg.fills.black;
+        }
+    }
 }
