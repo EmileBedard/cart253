2b7e05ba27d8aaa37c790af46ce72db9e0885288
EmileBedard <emile.bed@outlook.fr>
2025-10-30 16:06:06 -0400

    started the array loop to create new leaves. need to add the mouse press function to add the leaves and a function to actually draw the shapes and fill them with color

--- Projects/mod-jam/js/leaffloor.js
+++ Projects/mod-jam/js/leaffloor.js
@@ -15,13 +15,25 @@ let leaves = []; // this array will store the leaves we create
  * creates canvas HD resolution
 */
 function setup() {
-    createCanvas(1920, 1080)
+    createCanvas(1080, 1920)
 }
 
+let leaf = {
+    x: 30, // will be random later
+    y: 30, // will be random later
+    angle: 0, // will be random later
+    colorR: 100, // will be random between 60 and 200 to choose tint of leaf between yellow-ish and red-ish and everything in between
+}
 
 /**
  * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
 */
 function draw() {
-
+    for (let leaf of leaves) {
+        leaf.x = random(0, width);
+        leaf.y = random(0, height);
+        leaf.angle = random();
+        leaf.colorR = random(60, 200);
+        generateLeaf()
+    }
 }
