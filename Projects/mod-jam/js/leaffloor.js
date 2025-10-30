ff882201b02569f75ca6dd8f720321671e49d315
EmileBedard <emile.bed@outlook.fr>
2025-10-30 15:57:10 -0400

    started the leaffloor project that will let me create a texture to show behind the wood frogfrogfrog game! this will be fun

---
+++ Projects/mod-jam/js/leaffloor.js
@@ -0,0 +1,27 @@
+/**
+ * Leaf floor maker
+ * by emile bedard
+ * 
+ * This code creates random leaves at the click of a 
+ * button for a complete fall immersion, 
+ * let's celebrate the harvest season together
+ */
+
+"use strict";
+
+let leaves = []; // this array will store the leaves we create
+
+/**
+ * creates canvas HD resolution
+*/
+function setup() {
+    createCanvas(1920, 1080)
+}
+
+
+/**
+ * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+*/
+function draw() {
+
+}
