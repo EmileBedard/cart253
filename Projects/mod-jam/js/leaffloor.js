00b2724a63c705ad49283e2c2609d0a23ce617b9
EmileBedard <emile.bed@outlook.fr>
2025-10-30 16:27:55 -0400

    project is working real great! even added the leaves stems. now need to add the pointiness

--- Projects/mod-jam/js/leaffloor.js
+++ Projects/mod-jam/js/leaffloor.js
@@ -16,24 +16,55 @@ let leaves = []; // this array will store the leaves we create
 */
 function setup() {
     createCanvas(1080, 1920)
+    angleMode(DEGREES)
 }
 
-let leaf = {
-    x: 30, // will be random later
-    y: 30, // will be random later
-    angle: 0, // will be random later
-    colorR: 100, // will be random between 60 and 200 to choose tint of leaf between yellow-ish and red-ish and everything in between
+/**
+ * creates a new leaf
+ */
+function createLeaf() {
+
+    let newLeaf = {
+        x: random(0, width),
+        y: random(0, height),
+        angle: random(0, 360),
+        colorR: 190,
+        colorG: random(60, 200), //  between 60 and 200 to choose tint of leaf between yellow-ish and red-ish and everything in between
+        colorB: 0,
+    }
+    return newLeaf;
 }
 
 /**
  * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
 */
 function draw() {
-    for (let leaf of leaves) {
-        leaf.x = random(0, width);
-        leaf.y = random(0, height);
-        leaf.angle = random();
-        leaf.colorR = random(60, 200);
-        generateLeaf()
-    }
+    background(190, 150, 0);
+
+    for (let newLeaf of leaves) {
+        drawLeaf(newLeaf);
+    };
+}
+
+function drawLeaf(leafIndex) {
+    push();
+    fill(leafIndex.colorR, leafIndex.colorG, leafIndex.colorB);
+    noStroke();
+    ellipse(leafIndex.x, leafIndex.y, 250);
+    arc(leafIndex.x, leafIndex.y, 400, 400, leafIndex.angle, leafIndex.angle + 10);
+    pop();
+}
+
+// function generateLeaf(leafIndex) {
+//     leafIndex.x = random(0, width);
+//     leafIndex.y = random(0, height);
+//     leafIndex.angle = random();
+//     leafIndex.colorG = random(60, 200);
+// }
+
+/**
+ * adds a leaf to the array everytime the user click's mouse button
+ */
+function mousePressed() {
+    leaves.push(createLeaf());
 }
