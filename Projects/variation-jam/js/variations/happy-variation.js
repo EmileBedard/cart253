ac837024b49811d4ba30fd26ed28816d08a85ce6
EmileBedard <emile.bed@outlook.fr>
2025-11-27 10:49:23 -0500

    added a way to name the paintings you make EXTRA MEGA COOL AND FUN. i'm in love with this addition here. now i need sound. i have to stop being distracted

--- Projects/variation-jam/js/variations/happy-variation.js
+++ Projects/variation-jam/js/variations/happy-variation.js
@@ -28,6 +28,11 @@ let instructions = {
 let lastMoveTime = 0; // will be called whenever user mouse's moves.
 let inactivityDelay = 500; // 0.5 second
 
+let typedWord = ""
+let savedWord = ""
+
+let paintState = "naming"
+
 
 
 function happySetup() {
@@ -39,27 +44,47 @@ function happySetup() {
  * This will be called every frame when the pollock variation is active
  */
 function happyDraw() {
-    push();
-    // using movedX here to calculate the distance the mouse moved to fix the stroke weight
-    const weight = map(abs(movedX), 0, 30, 10, 6);
-    strokeWeight(weight);
 
-    // calculate stroke saturation here with the distance from the center. HSL color mode in this variation
-    // with this line, we set the distance in a variable called "d"
-    const d = dist(mouseX, mouseY, width / 2, height / 2);
+    if (paintState === "painting") {
 
-    changeHue(); // calls it here to check if the user is inactive and change strokeweight and hue before drawing the line
+        push();
+        // using movedX here to calculate the distance the mouse moved to fix the stroke weight
+        const weight = map(abs(movedX), 0, 30, 10, 6);
+        strokeWeight(weight);
 
-    // Set the stroke *saturation* based on the distance
-    color.saturation = map(d, -width / 2, width / 2, 0, 100);
-    stroke(color.hue, color.saturation, color.luminance, color.alpha);
+        // calculate stroke saturation here with the distance from the center. HSL color mode in this variation
+        // with this line, we set the distance in a variable called "d"
+        const d = dist(mouseX, mouseY, width / 2, height / 2);
 
-    // Draw a line from the previous mouse position to the current one AND add randomized position to have the "pollock" effect
-    line(pmouseX, pmouseY, mouseX + random(-40, 40), mouseY + random(-40, 40));
-    pop();
+        changeHue(); // calls it here to check if the user is inactive and change strokeweight and hue before drawing the line
 
-    drawInstructions(); // this calls the instructions to be drawn on top, m, n & s for different use
+        // Set the stroke *saturation* based on the distance
+        color.saturation = map(d, -width / 2, width / 2, 0, 100);
+        stroke(color.hue, color.saturation, color.luminance, color.alpha);
+
+        // Draw a line from the previous mouse position to the current one AND add randomized position to have the "pollock" effect
+        line(pmouseX, pmouseY, mouseX + random(-40, 40), mouseY + random(-40, 40));
+        pop();
+
+        drawInstructions(); // this calls the instructions to be drawn on top, m, n & s for different use
+    }
+
+    else {
+        background("#F8E5D0");
+
+        push();
+        fill(instructions.hue, instructions.saturation, instructions.luminance, instructions.alpha);
+        textAlign(CENTER, CENTER);
+        textSize(24);
+        textFont(pixelfont);
+        text("Name your masterpiece:", width / 2, height / 2);
+        text(typedWord, width / 2, 270);
+        pop();
+
+    }
+    console.log(paintState);
 }
+
 
 function changeHue() {
 
@@ -79,21 +104,61 @@ function drawInstructions() {
     pop();
 }
 
+function drawPaintingTitle() {
+
+    //draws the white rectangle to place the title
+    push();
+    fill('white');
+    noStroke();
+    rect(0, 420, 640, 60);
+    pop();
+
+    //draws the painting title
+    push();
+    fill(instructions.hue, instructions.saturation, instructions.luminance, instructions.alpha);
+    textAlign(CENTER, CENTER);
+    textSize(20);
+    textFont(pixelfont);
+    text(savedWord, width / 2, 450);
+    pop();
+
+}
+
 /**
  * This will be called whenever a key is pressed while the pollock variation is active
  */
 function happyKeyPressed(event) {
-    if (event.keyCode === 77) { // returns to main menu when "M" is pressed
-        state = "menu";
+    if (paintState === "painting") {
+
+        if (event.keyCode === 77) { // returns to main menu when "M" is pressed
+            state = "menu";
+        }
+
+        if (event.keyCode === 78) { // brings a new blank canvas when "N" is pressed
+            background("#F8E5D0");
+        }
+
+        if (event.keyCode === 83) { // saves the current canvas and downloads it when "s" is pressed! adds the name of the painting too. supersupersuper fun!
+            drawPaintingTitle();
+            saveCanvas('pollock_style_painting.png');
+            background("#F8E5D0");
+
+        }
     }
+    else {
 
+        // this section is for the naming of the pieces
 
-    if (event.keyCode === 78) { // brings a new blank canvas when "N" is pressed
-        background("#F8E5D0");
-    }
+        if (event.keyCode === 13) {
+            savedWord = typedWord;
+            typedWord = ""; // resets
+            paintState = "painting";
+            background("#F8E5D0");
+        }
 
-    if (event.keyCode === 83) { // saves the current canvas and downloads it when "s" is pressed!
-        saveCanvas('pollock_style_painting.png');
+        if (key.length === 1) {
+            typedWord += key;
+        }
     }
 }
 
